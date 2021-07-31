049ce6e5e63d0afbedc082c409136dcf5b180a9c
Jeff Hull <jeffkhull@github>
2021-07-31 14:05:54 -0400

    temp: update

--- src/feat/authn/services/AuthNService.ts
+++ src/feat/authn/services/AuthNService.ts
@@ -1,6 +1,28 @@
 import { reachNavigate } from '../../../common/util/navigate'
+import { ThreeIdConnect, EthereumAuthProvider } from '@3id/connect'
+import CeramicClient from '@ceramicnetwork/http-client'
+import { ConfigService } from '../../../common/config/config.service'
 
-export class AuthNService {
+export const ceramic = new CeramicClient(ConfigService.getConfig().ceramicHost)
+
+export class AuthenticationService {
+  static async authenticate() {
+    const w = window as any
+
+    if (!w.ethereum) {
+      throw new Error(`Ethereum not available in browser!`)
+    }
+
+    const addresses = await w.ethereum.enable()
+
+    const threeIdConnect = new ThreeIdConnect()
+    const authProvider = new EthereumAuthProvider(w.ethereum, addresses[0])
+    await threeIdConnect.connect(authProvider)
+    const provider = threeIdConnect.getDidProvider()
+    ceramic.did.setProvider(provider)
+    await ceramic.did.authenticate()
+  }
+
   static SaveRouteState = () => {
     console.log(`saving path ${window.location.pathname}`)
     sessionStorage.setItem('route-state', window.location.pathname)
